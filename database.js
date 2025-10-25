656014a4c2e983d6af65e54204710d96dd1118e2
Pramod Verma <vpramod103@gmail.com>
2025-10-25 20:52:27 +0530

    shifted mongoDB to online mongoDB cluster server

--- database.js
+++ database.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/hotels';
+// const mongoURL = 'mongodb://localhost:27017/hotels';
+//or
+// const mongoURL = process.env.mongoDB_Local;
+const mongoURL = process.env.DB_URL;
+
 
 mongoose.set('strictQuery', false);
 
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require ('express');
 const app = express();
 const db = require ('./database.js');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
     res.send("welcome to my hotel. How can i help you.");
